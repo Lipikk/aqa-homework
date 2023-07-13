727e80d2c94f72a403556d174a97af71f3e6c544
Elena <114442304+Lipikk@users.noreply.github.com>
2023-07-13 13:24:50 +0300

    lesson 2

--- lesson 2 Phyton/lesson_2_task_5.py
+++ lesson 2 Phyton/lesson_2_task_5.py
@@ -1,10 +1,15 @@
 # Месяц — сезон. Напишите функцию month_to_season(), которая принимает 1 аргумент — номер месяца — и возвращает название сезона, к которому относится этот месяц.
 # Например, передаем 2, на выходе получаем «Зима»
 
-def month_to_season():
-    print('Введите номер месяца: ')
+def month_to_season(month):
+    if month in (1, 2, 12): return 'Зима'
+    if month in (3, 4, 5): return 'Весна'
+    if month in (6, 7, 8): return 'Лето'
+    if month in (9, 10, 11): return 'Осень'
+    else: return 'Некорректный номер месяца'
 
+print(month_to_season(int(input("Введите номер месяца: "))))
 
-month_to_season()
+# готово
 
 
