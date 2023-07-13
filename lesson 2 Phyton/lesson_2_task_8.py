cee5213522aae9929e0780b9c8f6f690f8f7e321
Elena <114442304+Lipikk@users.noreply.github.com>
2023-07-13 13:16:10 +0300

    lesson 2

--- lesson 2 Phyton/lesson_2_task_8.py
+++ lesson 2 Phyton/lesson_2_task_8.py
@@ -1,7 +1,7 @@
 # Range. Создайте список [ 18, 14, 10, 6, 2 ] с помощью функции range() и выведите его на экран
 
-numbers = [ 18, 14, 10, 6, 2 ]
-for i in range (0, len(numbers)):
-  print(numbers[i])
+list = [ 18, 14, 10, 6, 2 ]
+for i in range (0, len(list)):
+  print(list[i])
 
 # готово
