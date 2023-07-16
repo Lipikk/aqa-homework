d0c6cea029bf8dcae0e6ace2c28b5dbc8ad09556
Elena <114442304+Lipikk@users.noreply.github.com>
2023-07-16 20:39:36 +0300

    lesson 2

--- lesson 3 Phyton/user.py
+++ lesson 3 Phyton/user.py
@@ -1,16 +1,10 @@
 # Создание класса
 
 class User:
-    def __init__(self, first_name, last_name ):
-        print("Я создался")
-        self.first_name = first_name
-        self.last_name = last_name
 
     def printName(self):
         print(self.first_name)
 
-    def printSurname(self):
-        print(self.last_name)
 
     def printNameSurname(self):
         print(self.first_name, self.last_name)
