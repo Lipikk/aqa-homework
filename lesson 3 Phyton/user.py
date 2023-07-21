aa9938bedd5ebfd97392b1a9a42173cbe0cae826
Elena <114442304+Lipikk@users.noreply.github.com>
2023-07-21 16:10:09 +0300

    lesson 3

--- lesson 3 Phyton/user.py
+++ lesson 3 Phyton/user.py
@@ -1,25 +1,37 @@
 # Создание класса
 
-class User:
+class User():
+    """в классе 3 метода которые печатают имя, фамилию и имя + фамилия """
     pass
 
-    def __init__(self,first_name, last_name):
-        self.Name = first_name
-        self.Surname = last_name  
+    def __init__(self, first_name, last_name):
+        """свойства юзера """
+        self.first_name = first_name
+        self.last_name = last_name 
     
     def getName(self):
-        return self.Name
+        print(self.first_name)
         
     def getSurname(self):
-        return self.Surname
+        print(self.last_name)
     
     def getPersonalInformation(self):
-        return self.Name, self.Surname
+        print(self.first_name, self.last_name)
+    
 
-person = User("Елена", "Mурзина")
+name1 = User("Eлена", "Мурзина")
+name2 = User("Elena", "Murzina")
 
+#print(name1.first_name)
+#print(name1.last_name)
 
+name1.getName()
+name1.getSurname()
+name1.getPersonalInformation()
 
+name2.getName()
+name2.getSurname()
+name2.getPersonalInformation()
 
     
 
